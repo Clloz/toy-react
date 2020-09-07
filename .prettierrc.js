88ded083cfd0e0a8a936a25fc8501ad666265abc
clloz <clloz@outlook.com>
2020-09-07 13:37:20 +0800

    add .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,29 @@
+/*
+ * @Author: Clloz
+ * @Date: 2020-09-04 22:43:46
+ * @LastEditTime: 2020-09-07 13:36:31
+ * @LastEditors: Clloz
+ * @Description:
+ * @FilePath: /toy-react/.prettierrc.js
+ * @博观而约取，厚积而薄发，日拱一卒，日进一寸，学不可以已。
+ */
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    jsxSingleQuote: false,
+    trailingComma: 'all',
+    quoteProps: 'as-needed',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: 'avoid',
+    requirePragma: false,
+    insertPragma: false,
+    proseWrap: 'preserve',
+    htmlWhitespaceSensitivity: 'ignore',
+    vueIndentScriptAndStyle: false,
+    endOfLine: 'auto',
+    // embeddedLanguageFormatting: "off"
+};
